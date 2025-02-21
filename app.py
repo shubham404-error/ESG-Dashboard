725890e522694ce6c56cca4638aef365b40c2e3c
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2025-02-21 13:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ elif menu == "Compare ESG Scores":
         if esg_data1 is not None and esg_data2 is not None:
             comparison_df = pd.DataFrame({
                 "Category": ['Environment', 'Social', 'Governance'],
-                ticker1: [esg_data1.loc['environmentScore'][0], esg_data1.loc['socialScore'][0], esg_data1.loc['governanceScore'][0]],
-                ticker2: [esg_data2.loc['environmentScore'][0], esg_data2.loc['socialScore'][0], esg_data2.loc['governanceScore'][0]]
+                ticker1: [esg_data1['environmentScore'].values[0], esg_data1['socialScore'].values[0], esg_data1['governanceScore'].values[0]],
+                ticker2: [esg_data2['environmentScore'].values[0], esg_data2['socialScore'].values[0], esg_data2['governanceScore'].values[0]]
             })
             st.write(comparison_df)
 
